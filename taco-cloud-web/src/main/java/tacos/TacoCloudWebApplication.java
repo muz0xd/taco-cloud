2d98c593004ce412865af093e7b96983cb5390a5
muz0xd <muz0xd@gmail.com>
2022-04-28 23:32:39 +0300

    web part creating

---
+++ taco-cloud-web/src/main/java/tacos/TacoCloudWebApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudWebApplication.class, args);
+	}
+
+}
