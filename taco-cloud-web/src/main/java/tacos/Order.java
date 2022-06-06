df331a05a71ec8f72597f9ab54b118d381b28701
muz0xd <muz0xd@gmail.com>
2022-06-06 22:05:16 +0300

    removed unused import statement

--- taco-cloud-web/src/main/java/tacos/Order.java
+++ taco-cloud-web/src/main/java/tacos/Order.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import lombok.Data;
 
 @Data
 public class Order {
