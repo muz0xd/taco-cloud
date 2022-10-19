29bc74ea6727174740f0899c339b689a6832b24a
muz0xd <muz0xd@gmail.com>
2022-10-19 22:20:32 +0300

    update Taco repo

--- taco-cloud-jpa/src/main/java/tacos/data/TacoRepository.java
+++ taco-cloud-jpa/src/main/java/tacos/data/TacoRepository.java
@@ -1,9 +1,8 @@
 package tacos.data;
 
 import tacos.Taco;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TacoRepository {
-	
-	Taco save (Taco design);
+public interface TacoRepository extends CrudRepository<Taco, Long>{
 
 }
