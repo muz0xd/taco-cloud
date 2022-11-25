f38edfbce59656cb5ff395bc710d100ec78e55ab
muz0xd <muz0xd@gmail.com>
2022-11-25 22:49:37 +0300

    init securing project

---
+++ taco-cloud-securing/src/main/java/tacos/TacoCloudSecuringApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSecuringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudSecuringApplication.class, args);
+	}
+
+}
