978619e041047404c1f729abe0e1552051f8c56d
muz0xd <muz0xd@gmail.com>
2022-12-18 18:45:28 +0300

    update secure config with user details service

--- taco-cloud-securing/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud-securing/src/main/java/tacos/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 
 @SuppressWarnings("deprecation")
@@ -14,6 +16,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Autowired 
+    private UserDetailsService userDetailsService;
+    
+    @Bean
+    public PasswordEncoder encoder() {
+      return new StandardPasswordEncoder("53cr3t");
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
+      
+    }
+    
 //
 //  IN MEMORY AUTHENTICATION
 //
@@ -37,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //
 //  JDBC AUTHENTICATION
 //
-    @Autowired
+/*    @Autowired
     DataSource dataSource;
     
     @Override
@@ -47,4 +66,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         .jdbcAuthentication()
         .dataSource(dataSource);
     }
+*/
 }
