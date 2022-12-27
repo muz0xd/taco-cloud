badd25c3f187fb43874123eaebf42c2ff5fad410
muz0xd <muz0xd@gmail.com>
2022-12-27 22:28:12 +0300

    inject user and order model to get controller

--- taco-cloud-securing/src/main/java/tacos/web/OrderController.java
+++ taco-cloud-securing/src/main/java/tacos/web/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -27,7 +28,8 @@ public class OrderController {
 	}
   
 	@GetMapping("/current")
-	public String orderForm() {
+	public String orderForm(@AuthenticationPrincipal User user, 
+	        @ModelAttribute Order order) {
 		return "orderForm";
 	}
 
