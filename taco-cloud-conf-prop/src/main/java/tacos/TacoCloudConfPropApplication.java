fa8ab1d506d70ae4608cdbe31b359c26de84f228
muz0xd <muz0xd@gmail.com>
2023-02-07 22:27:05 +0300

    initial commit for configuration properties project

---
+++ taco-cloud-conf-prop/src/main/java/tacos/TacoCloudConfPropApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudConfPropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudConfPropApplication.class, args);
+	}
+
+}
