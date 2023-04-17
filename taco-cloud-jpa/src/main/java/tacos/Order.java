d2840aef4e71bd2727eef412ecdb8c99147e6098
muz0xd <muz0xd@gmail.com>
2023-04-17 20:44:19 +0300

    code corrective to prove

--- taco-cloud-jpa/src/main/java/tacos/Order.java
+++ taco-cloud-jpa/src/main/java/tacos/Order.java
@@ -57,6 +57,7 @@ public class Order implements Serializable {
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
 
+	//TODO: OneToMany relation is correct variant! need to prove later.
 	@ManyToMany(targetEntity=Taco.class)
 	private List<Taco> tacos = new ArrayList<>();
   
