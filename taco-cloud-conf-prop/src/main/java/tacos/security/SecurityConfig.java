7fc56787ff3051fa306b461a1fa9d97d4d2c47a2
muz0xd <muz0xd@gmail.com>
2023-04-17 21:07:28 +0300

    add previous java code

---
+++ taco-cloud-conf-prop/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,99 @@
+package tacos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    
+    
+    @Autowired 
+    @Lazy // avoid circular references
+    private UserDetailsService userDetailsService;
+    
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("53cr3t");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .authorizeRequests()
+            .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+            .antMatchers("/", "/**").access("permitAll")
+        .and()
+            .formLogin()
+                .loginPage("/login")
+        .and()
+            .logout()
+                .logoutSuccessUrl("/")
+        .and()
+            .csrf()
+            .ignoringAntMatchers("/h2-console/**")
+         // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+        .and()  
+            .headers()
+            .frameOptions()
+            .sameOrigin()
+        ;
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
+      
+    }
+    
+//
+//  IN MEMORY AUTHENTICATION
+//
+    /*
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+        .inMemoryAuthentication()
+        .withUser("buzz")
+        .password("{noop}infinity") // Password Storage Format {id}encodedPassword: this id delegate to NoOpPasswordEncoder
+        .authorities("ROLE_USER")
+        .and()
+        .withUser("woody")
+        .password("{sha256}97cde38028ad898ebc02e690819fa220e88c62e0"
+                + "699403e94fff291cfffaf8410849f27605abcbc0") // "password" encoded with sha256
+        .authorities("ROLE_USER");
+    }
+    */
+
+//
+//  JDBC AUTHENTICATION
+//
+/*    @Autowired
+    DataSource dataSource;
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) 
+            throws Exception {
+        auth
+        .jdbcAuthentication()
+        .dataSource(dataSource);
+    }
+*/
+}
---
+++ taco-cloud-conf-prop/src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,68 @@
+package tacos.web;
+
+import javax.validation.Valid;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import tacos.Order;
+import tacos.User;
+import tacos.data.OrderRepository;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+	  
+	private OrderRepository orderRepo;
+
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+  
+	@GetMapping("/current")
+	public String orderForm(@AuthenticationPrincipal User user, 
+	        @ModelAttribute Order order) {
+	    if (order.getName() == null) {
+	        order.setName(user.getFullname());
+	    }
+	    if (order.getStreet() == null) {
+	        order.setStreet(user.getStreet());
+	    }
+	    if (order.getCity() == null) {
+	        order.setCity(user.getCity());
+	    }
+	    if (order.getState() == null) {
+	        order.setState(user.getState());
+	    }
+	    if (order.getZip() == null) {
+	        order.setZip(user.getZip());
+	    }
+	      
+		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(@Valid Order order, Errors errors, 
+			SessionStatus sessionStatus,
+			@AuthenticationPrincipal User user) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
+		order.setUser(user);
+    
+		orderRepo.save(order);
+		sessionStatus.setComplete();
+    
+		return "redirect:/";
+	}
+
+}
