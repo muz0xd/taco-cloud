a82fc5b1f5443844d4b662da7bdbff183a103456
muz0xd <muz0xd@gmail.com>
2023-04-19 21:09:35 +0300

    user orders find method added

--- taco-cloud-conf-prop/src/main/java/tacos/data/OrderRepository.java
+++ taco-cloud-conf-prop/src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,15 @@
 package tacos.data;
 
 import tacos.Order;
+import tacos.User;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long>{
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 	
 }
