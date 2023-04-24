0162c9801472d1c1ff17c8738533e6e179fe4028
muz0xd <muz0xd@gmail.com>
2023-04-24 21:15:40 +0300

    add profile ability

--- taco-cloud-conf-prop/src/main/java/tacos/TacoCloudConfPropApplication.java
+++ taco-cloud-conf-prop/src/main/java/tacos/TacoCloudConfPropApplication.java
@@ -1,14 +1,7 @@
 package tacos;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import tacos.Ingredient.Type;
-import tacos.data.IngredientRepository;
-import tacos.data.UserRepository;
 
 @SpringBootApplication
 public class TacoCloudConfPropApplication {
@@ -16,32 +9,4 @@ public class TacoCloudConfPropApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudConfPropApplication.class, args);
 	}
-	
-	@Bean
-	public CommandLineRunner dataLoader(IngredientRepository repo,
-	        UserRepository userRepo, PasswordEncoder encoder) {
-	    return new CommandLineRunner() {
-	        @Override
-	        public void run(String... args) throws Exception {
-	            repo.deleteAll();
-	            userRepo.deleteAll();
-	            
-	            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-	            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-	            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-	            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-	            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-	            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-	            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-	            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-	            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-	            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-	            
-	            userRepo.save(new User("user", encoder.encode("password"), 
-	                    "Default User", "123 North Street", "Cross Roads", "TX", 
-	                    "76227", "123-123-1234"));
-	        }
-	    };
-	}
-
 }
