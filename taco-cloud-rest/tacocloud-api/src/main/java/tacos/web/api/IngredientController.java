0ad893dd8e3825b6f4d22978c746854c4541a89c
muz0xd <muz0xd@gmail.com>
2023-12-09 13:00:44 +0300

    formatting

--- taco-cloud-rest/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
+++ taco-cloud-rest/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
@@ -1,6 +1,5 @@
 package tacos.web.api;
 
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +13,24 @@ import tacos.Ingredient;
 import tacos.data.IngredientRepository;
 
 @RestController
-@RequestMapping(path="/api/ingredients", produces="application/json")
-@CrossOrigin(origins="http://localhost:8080")
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
 public class IngredientController {
 
-  private IngredientRepository repo;
+    private IngredientRepository repo;
 
-  @Autowired
-  public IngredientController(IngredientRepository repo) {
-    this.repo = repo;
-  }
+    @Autowired
+    public IngredientController(IngredientRepository repo) {
+        this.repo = repo;
+    }
 
-  @GetMapping
-  public Iterable<Ingredient> allIngredients() {
-    return repo.findAll();
-  }
-  
-  @GetMapping("/{id}")
-  public Optional<Ingredient> IngredientById(@PathVariable("id") String id) {
-    return repo.findById(id);
-  }
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return repo.findAll();
+    }
 
+    @GetMapping("/{id}")
+    public Optional<Ingredient> IngredientById(@PathVariable("id") String id) {
+        return repo.findById(id);
+    }
 }
