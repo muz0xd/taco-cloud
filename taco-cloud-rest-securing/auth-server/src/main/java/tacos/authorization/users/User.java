aa84493345b3bc40ef9b3abc9886ef86ed8e5de3
muz0xd <muz0xd@gmail.com>
2024-04-18 13:54:57 +0300

    standard fix allready

--- taco-cloud-rest-securing/auth-server/src/main/java/tacos/authorization/users/User.java
+++ taco-cloud-rest-securing/auth-server/src/main/java/tacos/authorization/users/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Data
 @Entity
 @RequiredArgsConstructor
+@Table(name="users") // user is reserved SQL word
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class User implements UserDetails {
 
