cdc8e93acadb86887c0e3bd27110ebdd6d3f2f2d
muz0xd <muz0xd@gmail.com>
2024-09-26 22:47:06 +0300

    format file

--- taco-cloud-rest-securing/tacocloud/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
+++ taco-cloud-rest-securing/tacocloud/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
@@ -16,32 +16,32 @@ import tacos.Ingredient;
 import tacos.data.IngredientRepository;
 
 @RestController
-@RequestMapping(path="/api/ingredients", produces="application/json")
-@CrossOrigin(origins="http://localhost:8080")
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
 public class IngredientController {
 
-  private IngredientRepository repo;
+	private IngredientRepository repo;
 
-  @Autowired
-  public IngredientController(IngredientRepository repo) {
-    this.repo = repo;
-  }
+	@Autowired
+	public IngredientController(IngredientRepository repo) {
+		this.repo = repo;
+	}
 
-  @GetMapping
-  public Iterable<Ingredient> allIngredients() {
-    return repo.findAll();
-  }
+	@GetMapping
+	public Iterable<Ingredient> allIngredients() {
+		return repo.findAll();
+	}
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
-    return repo.save(ingredient);
-  }
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+		return repo.save(ingredient);
+	}
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteIngredient(@PathVariable("id") String ingredientId) {
-    repo.deleteById(ingredientId);
-  }
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteIngredient(@PathVariable("id") String ingredientId) {
+		repo.deleteById(ingredientId);
+	}
 
 }
