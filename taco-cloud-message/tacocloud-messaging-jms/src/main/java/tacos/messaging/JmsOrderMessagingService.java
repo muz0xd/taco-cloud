3eccb7227ea4ece4c6311ddda6369e1a5df9e064
muz0xd <muz0xd@gmail.com>
2024-10-29 20:50:22 +0300

    code format

--- taco-cloud-message/tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
+++ taco-cloud-message/tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -12,22 +12,21 @@ import tacos.TacoOrder;
 @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
 
-  private JmsTemplate jms;
+	private JmsTemplate jms;
 
-  @Autowired
-  public JmsOrderMessagingService(JmsTemplate jms) {
-    this.jms = jms;
-  }
+	@Autowired
+	public JmsOrderMessagingService(JmsTemplate jms) {
+		this.jms = jms;
+	}
 
-  @Override
-  public void sendOrder(TacoOrder order) {
-    jms.convertAndSend("tacocloud.order.queue", order,
-        this::addOrderSource);
-  }
-  
-  private Message addOrderSource(Message message) throws JMSException {
-    message.setStringProperty("X_ORDER_SOURCE", "WEB");
-    return message;
-  }
+	@Override
+	public void sendOrder(TacoOrder order) {
+		jms.convertAndSend("tacocloud.order.queue", order, this::addOrderSource);
+	}
+
+	private Message addOrderSource(Message message) throws JMSException {
+		message.setStringProperty("X_ORDER_SOURCE", "WEB");
+		return message;
+	}
 
 }
